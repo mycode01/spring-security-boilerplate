43be618bc1ef741a8fd9b66bf8bf1d9e50894b1b
mycode01@hotmail.com <mycode01@hotmail.com>
2019-06-25 17:35:00 +0900

    jpa commit

---
+++ jpa/src/main/resources/data-local.sql
@@ -0,0 +1,12 @@
+select 1;
+
+insert into member
+(ID, IS_ENABLED, ROLES, UID, UPW, USERNAME)
+values
+(null, 'Y', 'USER', 'user', '{noop}123', '테스터')
+;
+insert into member
+(ID, IS_ENABLED, ROLES, UID, UPW, USERNAME)
+values
+(null, 'Y', 'USER,ADMIN', 'admin', '{noop}123', '어드민')
+;
